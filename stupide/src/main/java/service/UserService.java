53519d8cc784b6edb15938fdab9e0a76e652b177
gogowrong <15528839300@163.com>
2018-09-10 19:57:48 +0800

    增加了一个java文件

---
+++ stupide/src/main/java/service/UserService.java
@@ -0,0 +1,12 @@
+package service;
+
+/**
+ * Created by Administrator on 2018/9/10.
+ */
+public class UserService {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+
+}
+
